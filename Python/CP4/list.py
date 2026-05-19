5315ce4ad37408605e4832cd7bd92c21f36f5273
imshubhamgiri <skgiri569@gmail.com>
2026-05-19 23:39:51 +0530

    learn about lists in python

---
+++ Python/CP4/list.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "orange"]  ##this is a list of fruits
+fruits[2] = 244.44  ##this will change the third element of the list to 244.44
+print(fruits[0])  ##this will print the first element of the list, which is "apple"
+print(fruits[2])  ##this will print the third element of the list, which is 244.44
